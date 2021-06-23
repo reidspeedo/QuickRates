1982e1fb58000fbf3226ae7f2e9002e5ef1c83d4
Reid Relatores <rrrelatores@gmail.com>
2021-06-23 17:22:51 -0600

    im dumb again

--- repository/model.py
+++ repository/model.py
@@ -3,7 +3,8 @@ import os
 import pymongo
 import time
 
-connection_string = os.environ.get("MONGO_DB_CONN_STR")
+connection_string = 'mongodb+srv://mainuser:quickrate@cluster0.bbjks.mongodb.net/quickrate?retryWrites=true&w=majority'
+# connection_string = os.environ.get("MONGO_DB_CONN_STR")
 client = pymongo.MongoClient(connection_string)
 db = client.quickrate
 
