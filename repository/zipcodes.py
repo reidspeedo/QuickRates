ca89dd4ccabbc9a8101551503d0faf7c88d33f41
Reid Relatores <rrrelatores@gmail.com>
2021-06-23 17:19:28 -0600

    im dumb

--- repository/zipcodes.py
+++ repository/zipcodes.py
@@ -1,9 +1,11 @@
 import pymongo
 import os
 
-connection_string = os.environ.get("MONGO_DB_CONN_STR")
+connection_string = 'mongodb+srv://mainuser:quickrate@cluster0.bbjks.mongodb.net/quickrate?retryWrites=true&w=majority'
+# connection_string = os.environ.get("MONGO_DB_CONN_STR")
 client = pymongo.MongoClient(connection_string)
 db = client.quickrate
+
 
 def get_avg_pre_zip(zip):
     return db.zipcodes.find({'zip': zip})
