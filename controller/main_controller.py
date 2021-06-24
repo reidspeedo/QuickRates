3595c6fdcd7c6052d0f2221a7fe1c0402d4457d1
Reid Relatores <rrrelatores@gmail.com>
2021-06-23 21:56:03 -0600

    update CORS policy

--- controller/main_controller.py
+++ controller/main_controller.py
@@ -9,6 +9,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",
+    "https://quiet-wave-93259.herokuapp.com/",
 ]
 
 app.add_middleware(
--- linear_model/linear_model.py
+++ linear_model/linear_model.py
@@ -6,9 +6,12 @@ from sklearn import linear_model
 import pymongo
 import os
 
-connection_string = os.environ.get("MONGO_DB_CONN_STR")
+connection_string = 'mongodb+srv://mainuser:quickrate@cluster0.bbjks.mongodb.net/quickrate?retryWrites=true&w=majority'
+
+# connection_string = os.environ.get("MONGO_DB_CONN_STR")
 client = pymongo.MongoClient(connection_string)
 db = client.quickrate
+
 
 def retrieve_state_zips(file, state):
     az = pd.read_csv(file)
